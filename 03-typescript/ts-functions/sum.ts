f700903a56687570ddf14794f5a91cc75dbd147e
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-12 17:27:29 +0100

    more practice ts-functions

--- 03-typescript/ts-functions/sum.ts
+++ 03-typescript/ts-functions/sum.ts
@@ -1,6 +1,6 @@
 'use strict';
 // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
-let num: number = 10;
+let num: number = 5;
 function sum(num){
   for (let i: number = num - 1; i > 0; i--){
     num += i;
