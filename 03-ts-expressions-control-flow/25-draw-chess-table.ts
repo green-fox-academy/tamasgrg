ebdece566dd1379c3290e5de380fe44f88cc0508
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-22 19:12:28 +0100

    finished 26-substr

--- 03-ts-expressions-control-flow/25-draw-chess-table.ts
+++ 03-ts-expressions-control-flow/25-draw-chess-table.ts
@@ -16,7 +16,7 @@ let s: string = ' ';
 for (let i: number = 1; i <= 8; i++) {
   let str: string = '';
   for (let j: number = 1; j <= 8; j++) {
-    if ((i % 2 != 0 && j % 2 != 0) || (i % 2 === 0 && j % 2 === 0)){
+    if ((i % 2 != 0 && j % 2 != 0) || (i % 2 === 0 && j % 2 === 0)) {
       str += k;    
     } else {
       str += s;
