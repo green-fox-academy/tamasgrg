76296df38af13d48940b01de8f7c47d20ef4ab0d
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-22 19:01:28 +0100

    finished 25-draw-chess-table

--- 03-ts-expressions-control-flow/25-draw-chess-table.ts
+++ 03-ts-expressions-control-flow/25-draw-chess-table.ts
@@ -1,5 +1,4 @@
 'use strict';
-
 // Create a program that draws a chess table like this
 //
 // % % % %
@@ -11,4 +10,18 @@
 // % % % %
 //  % % % %
 //
+let k: string = '%';
+let s: string = ' ';
+
+for (let i: number = 1; i <= 8; i++) {
+  let str: string = '';
+  for (let j: number = 1; j <= 8; j++) {
+    if ((i % 2 != 0 && j % 2 != 0) || (i % 2 === 0 && j % 2 === 0)){
+      str += k;    
+    } else {
+      str += s;
+    }
+  }
+  console.log(str);
+}
 export{};
