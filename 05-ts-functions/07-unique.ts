cb02fcc7b483e6bec5d3ede0602c40c0d9f4c4aa
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-23 15:52:33 +0100

    finished 07-unique

--- 05-ts-functions/07-unique.ts
+++ 05-ts-functions/07-unique.ts
@@ -1,10 +1,16 @@
 'use strict';
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
-
-function unique(arr) {
-
-}
+function unique(arr: number[]): number[] {
+  let arr2: number[] = [];
+  arr.forEach((value: number, index: number) => {
+    arr.sort();
+    if (arr[index] != arr[index + 1]) {
+      arr2.push(arr[index]);
+    }
+  })
+  return arr2;
+};
 
 //  Example
 console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
