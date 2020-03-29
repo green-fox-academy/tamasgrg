09b3174cd423f10af1fdc1871c781a32f1800e95
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-29 16:00:39 +0200

    finished 09-subint

--- 05-ts-functions/09-subint.ts
+++ 05-ts-functions/09-subint.ts
@@ -2,9 +2,19 @@
 //  Create a function that takes a number and an array of numbers as a parameter
 //  Returns the indices of the numbers in the array of which the first number is a part of
 //  Or returns an empty list if the number is not part of any of the numbers in the array
+function subint (number: number, list: number[]): number[] {
+  let result: number[] = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].toString().includes(number.toString())) {
+      result.push(i);
+    }
+  }
+  return result;
+}
 
 // Example
 console.log(subint(1, [1, 11, 34, 52, 61]));
 // should print: `[0, 1, 4]`
 console.log(subint(9, [1, 11, 34, 52, 61]));
 // should print: '[]'
+export{};
