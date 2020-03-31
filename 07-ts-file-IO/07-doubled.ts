c7107b23270a44454634d50ddad6165c450c94cf
Tamas Gorog <tjejtjusare@gmail.com>
2020-03-31 15:46:51 +0200

    finished 07-doubled

--- 07-ts-file-IO/07-doubled.ts
+++ 07-ts-file-IO/07-doubled.ts
@@ -1,5 +1,5 @@
-'use strict';
 // Create a method that decrypts duplicated-chars.txt
+'use strict';
 const fs = require('fs');
 
 function readFile(filePath: string): string {
@@ -12,7 +12,8 @@ function readFile(filePath: string): string {
   return fileContent;
 }
 
-function halfLetters(array: string[]) {
+//solution #1 (write to file ok, but console.log prints lines on top of each other)
+function halfLetters(array: string[]){
   let halfLetters: string[] = [];
   for (let i = 0; i < array.length; i += 2) {
     halfLetters.push(array[i]);
@@ -20,15 +21,47 @@ function halfLetters(array: string[]) {
   return halfLetters;
 }
 
-function decodeText(filePath) {
+function decodeText(filePath: string): string {
   const origiText: string = readFile(filePath);
   const allLettersArray: string[] = origiText.split('');
   const halfLettersArray: string[] = halfLetters(allLettersArray);
   const readyText: string = halfLettersArray.join('');
   return readyText;
 }
+// console.log(decodeText('files/07-duplicated-chars.txt'))
+// fs.writeFileSync('files/07-decoded-text.txt', decodeText('files/07-duplicated-chars.txt'));
 
-// console.log doesn't work, write to file does   ?????????????
-console.log(decodeText('files/07-duplicated-chars.txt'))
-fs.writeFileSync('files/07-decoded-text.txt', decodeText('files/07-duplicated-chars.txt'));
-export {};
+
+//solution #2
+
+function singleChars(text: string[]): string {
+  let singleText: string[] = [];
+  text.forEach((value: string, index: number) => {
+    singleText.push(value
+    .split('')
+    .filter((value: string, index: number) => index % 2 === 0)
+    .join(''))
+  })
+  return singleText.join('');
+}
+
+function writeFile(outPath: string, fileContent: string): void {
+  try {
+    fs.writeFileSync(outPath, fileContent);
+    console.log('File write successfull...');
+  } catch (e) {
+    console.log('Error while writing file...');
+  }
+}
+
+function decodeText2(inPath: string, outPath: string): string {
+  const text: string = readFile(inPath);
+  const textLines: string[] = text.split('\r');
+  const decodedText: string = singleChars(textLines);
+  writeFile(outPath, decodedText);
+  return decodedText;
+}
+
+// console.log(decodeText2('files/07-duplicated-chars.txt', 'files/07-decoded-text.txt'));
+decodeText2('files/07-duplicated-chars.txt', 'files/07-decoded-text.txt');
+export{};
