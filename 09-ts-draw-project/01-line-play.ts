9355f17d4a9382b6bd3904bcc87339e20f73ec0a
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-02 17:57:23 +0200

    finished 01-line-play

---
+++ 09-ts-draw-project/01-line-play.ts
@@ -0,0 +1,19 @@
+'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+for (let i = 0; i < 400; i = i + 20) {  
+  ctx.beginPath();
+  ctx.moveTo(i, 400);
+  ctx.lineTo(0, i);
+  ctx.stroke();
+  ctx.strokeStyle = '#00cd00';
+}
+
+for (let j = 0; j < 400; j = j + 20) {  
+  ctx.beginPath();
+  ctx.moveTo(400, j);
+  ctx.lineTo(j, 0);
+  ctx.stroke();
+  ctx.strokeStyle = '#a900f2';
+}
