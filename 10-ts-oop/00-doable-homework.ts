c04862da2fd4fba25ce1efc39284135a9bc24a44
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-03 12:41:21 +0200

    finished 00-doable-hw

---
+++ 10-ts-oop/00-doable-homework.ts
@@ -0,0 +1,49 @@
+// Create 5 trees
+// Store the data of them in variables in your program
+// for every tree the program should store its'
+// type
+// leaf color
+// age
+// sex
+// you can use just variables, or lists and/or maps
+
+// Lekvár plum green 33 male
+// Torta lemon green 4 male
+// Pálinka nut green 2 female
+// Befőtt peach green 20 female 
+// Szirup maple red 13 female
+
+let Lekvar: object = {
+  'type': 'plum',
+  'leaf color': 'green',
+  'age': 33,
+  'sex': 'male'
+}
+
+let Torta: object = {
+  'type': 'lemon',
+  'leaf color': 'green',
+  'age': 4,
+  'sex': 'male'
+}
+
+let Palinka: object = {
+  'type': 'nut',
+  'leaf color': 'green',
+  'age': 2,
+  'sex': 'male'
+}
+
+let Befott: object = {
+  'type': 'peach',
+  'leaf color': 'green',
+  'age': 20,
+  'sex': 'female'
+}
+
+let Szirup: object = {
+  'type': 'maple',
+  'leaf color': 'green',
+  'age': 13,
+  'sex': 'female'
+}
