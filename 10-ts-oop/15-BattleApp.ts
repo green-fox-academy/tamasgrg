1c78bec872bda6d8b5c9e4602cbb7e95dcbd7b2c
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-11 17:50:12 +0200

    fixed BattleApp.ts

--- 10-ts-oop/15-BattleApp.ts
+++ 10-ts-oop/15-BattleApp.ts
@@ -4,17 +4,17 @@ import {Ship} from './15-Ship';
 
 export class BattleApp {
 
+  static battle(ship1: Ship, ship2: Ship): void {
+    ship1.fillShip();
+    ship2.fillShip();
+    ship1.printStateOfShip();
+    ship2.printStateOfShip();
+    ship1.battle(ship2)
+    ship1.printStateOfShip();
+    ship2.printStateOfShip();
+  }
 }
 
 let ship1 = new Ship;
 let ship2 = new Ship;
-ship1.fillShip();
-ship2.fillShip();
-ship1.captain.drinkSomeRum();
-ship2.captain.drinkSomeRum();
-ship1.printStateOfShip();
-ship2.printStateOfShip();
-// console.log(ship);
-ship1.battle(ship2)
-ship1.printStateOfShip();
-ship2.printStateOfShip();
+BattleApp.battle(ship1, ship2);
