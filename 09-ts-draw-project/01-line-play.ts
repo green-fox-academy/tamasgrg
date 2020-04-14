0625c85fc683f94e1f1ee78aa2924558f82f8e3a
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-14 12:50:26 +0200

    finished 01-line-play

---
+++ 09-ts-draw-project/01-line-play.ts
@@ -0,0 +1,24 @@
+'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+const sizeCanvas: number = 400;
+const marginCanvas: number = 10;
+const lineNumber: number = 15;
+const lineDistance: number = (sizeCanvas - 2 * marginCanvas) / lineNumber;
+
+ctx.strokeStyle = '#00cd00';
+ctx.beginPath();
+for (let i = 1; i < lineNumber; i++) {  
+  ctx.moveTo(i * lineDistance, sizeCanvas - marginCanvas);
+  ctx.lineTo(marginCanvas, i * lineDistance);
+}
+ctx.stroke();
+
+ctx.strokeStyle = '#a900f2';
+ctx.beginPath();
+for (let j = 1; j < lineNumber; j ++) {  
+  ctx.moveTo(j * lineDistance, marginCanvas);
+  ctx.lineTo(sizeCanvas - marginCanvas, j * lineDistance);
+}
+ctx.stroke();
