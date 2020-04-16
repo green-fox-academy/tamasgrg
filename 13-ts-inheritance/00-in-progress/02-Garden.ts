a1931e6e0dd77f7eb3638b9a8cfcd61ed262daa5
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-16 18:13:19 +0200

    init commit 02-garden-app

---
+++ 13-ts-inheritance/00-in-progress/02-Garden.ts
@@ -0,0 +1,12 @@
+'use strict';
+import { Flower } from "./02-Flower";
+import { Tree } from "./02-Tree";
+// The Garden
+// is able to hold unlimited amount of flowers or trees
+// when watering it should only water those what needs water with equally divided amount
+//  amongst them
+// eg. watering with 40 and 4 of them need water then each gets watered with 10
+
+export class Garden {
+
+}
