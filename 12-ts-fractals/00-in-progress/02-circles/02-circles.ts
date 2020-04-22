fb3949ff71f8ad08730dddea1646a7d5e1247b8a
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-22 18:08:22 +0200

    initial commit of 02-circles

---
+++ 12-ts-fractals/00-in-progress/02-circles/02-circles.ts
@@ -0,0 +1,6 @@
+'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+const sizeCanvasX: number = 600;
+const sizeCanvasY: number = 600;
