a15fdee51c61982ff032bcdea06a55e15d04fae7
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-22 14:14:25 +0200

    finished 01-apples

---
+++ 15-ts-testing/operations.ts
@@ -0,0 +1,17 @@
+export function addNum(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtractNum(a: number, b: number): number {
+  return a - b;
+}
+
+export class NumberOperations {
+  public addNum(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  public subtractNum(num1: number, num2: number): number {
+    return num1 - num2;
+  }
+}
---
+++ 15-ts-testing/test.ts
@@ -0,0 +1,15 @@
+import * as test from 'tape';
+import { addNum, subtractNum } from './operations';
+
+test('add 2 numbers', t => {
+  const actual = addNum(1, 2);
+  const expected = 3;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('subtract 2 numbers', t => {
+  t.equal(subtractNum(2, 1), 1);
+  t.end();
+});
