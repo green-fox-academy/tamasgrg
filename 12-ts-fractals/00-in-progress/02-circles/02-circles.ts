c817b5599ae8d3a43406cb522585b3c4ef038fbd
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-22 18:19:53 +0200

    initial commit of 02-circles

--- 12-ts-fractals/00-in-progress/02-circles/02-circles.ts
+++ 12-ts-fractals/00-in-progress/02-circles/02-circles.ts
@@ -4,3 +4,13 @@ const ctx = canvas.getContext('2d');
 
 const sizeCanvasX: number = 600;
 const sizeCanvasY: number = 600;
+
+function drawCircle(x: number, y: number, r: number) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.stroke();
+}
+
+drawCircle(300, 300, 300)
+
+drawCircle(300, 150, 150)
