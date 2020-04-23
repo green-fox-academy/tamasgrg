c9ac4b9927f497badbfd8adea4645e02df7471ed
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-23 14:18:02 +0200

    calculations ok, working on the logic

--- 12-ts-fractals/00-in-progress/02-circles/02-circles.ts
+++ 12-ts-fractals/00-in-progress/02-circles/02-circles.ts
@@ -11,6 +11,19 @@ function drawCircle(x: number, y: number, r: number) {
   ctx.stroke();
 }
 
-drawCircle(300, 300, 300)
-
-drawCircle(300, 150, 150)
+fractalCircles(300, 300, 300);
+function fractalCircles(x: number, y: number, r: number) {
+  // const a: number = r/2*Math.sqrt(3);
+  // const m: number = Math.sqrt(3) * a / 2;
+  // const b: number = m - r;
+  // if (r>25) {
+  drawCircle(x, y, r);
+  // fractalCircles(x, y/2, r / 2);
+  // fractalCircles(x-a/2, y+b, r / 2);
+  // fractalCircles(x+a/2, y+b, r / 2);
+  // }
+let a = 150*Math.sqrt(3) 
+drawCircle(300, 150, 150);
+drawCircle(300-a/2, 300+75, 150);
+drawCircle(300+a/2, 300+75, 150);
+}
