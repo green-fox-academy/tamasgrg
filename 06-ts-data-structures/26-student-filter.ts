ed80838eaf845db7b589242cb3b46cad5d97c6d0
Tamas Gorog <tjejtjusare@gmail.com>
2020-04-29 17:45:38 +0200

    finished 26-student-filter

---
+++ 06-ts-data-structures/26-student-filter.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+const students: any[] = [
+  { name: 'Mark', age: 9.5, candies: 2 },
+  { name: 'Paul', age: 12, candies: 5 },
+  { name: 'Clark', age: 7, candies: 3 },
+  { name: 'Pierce', age: 12, candies: 7 },
+  { name: 'Sean', age: 10, candies: 1 }
+];
+
+// create a function that takes a list of students and logs:
+// - Who has got more candies than 4 candies
+function studentsWhoOwnMoreThan4CandiesCounter(list: any[]): string[] {
+  let studentsWhoOwnMoreThan4Candies: string[] = [];
+  list.forEach((value: any) => {
+    if (value.candies > 4) studentsWhoOwnMoreThan4Candies.push(value.name);
+  });
+  studentsWhoOwnMoreThan4Candies.forEach((value: string) => console.log(value));
+  return studentsWhoOwnMoreThan4Candies;
+}
+studentsWhoOwnMoreThan4CandiesCounter((students));
+
+// create a function that takes a list of students and logs: 
+//  - how many candies they have on average
+function avgCandies(list: any[]): number {
+  let totalCandies: number = 0;
+  list.forEach((value: any) => totalCandies += value.candies);
+  let avgCandies: number = totalCandies / list.length;
+  console.log(avgCandies);
+  return avgCandies;
+}
+avgCandies(students);
+
+export{};
