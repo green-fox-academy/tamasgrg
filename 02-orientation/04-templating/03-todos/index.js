a2edcb46104206e8f73f628928150a248ebdedff
tamasgrg <gorog@consolidity.com>
2020-06-11 20:24:46 +0200

    03-todos done

---
+++ 02-orientation/04-templating/03-todos/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const todos = ['get up', 'survive', 'go back to bed'];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('home', { todos }));
+  
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
