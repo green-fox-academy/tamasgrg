3f06552b0cbe1bed3c5494240311570ef742e3b2
tamasgrg <gorog@consolidity.com>
2020-06-13 15:14:56 +0200

    all navigation-logic works

--- 02-orientation/05-gallery-project/gallery/db.js
+++ 02-orientation/05-gallery-project/gallery/db.js
@@ -3,49 +3,49 @@ const picDb = [
         title: 'Sharp turn',
         picSrc: 'pic00.jpg',
         thSrc: 'th00.jpg',
-        descripition: 'Kropa is taking a sharp turn to lead an attack',
+        description: 'Kropa is taking a sharp turn to lead an attack',
     },
     {
         title: 'Fight',
         picSrc: 'pic01.jpg',
         thSrc: 'th01.jpg',
-        descripition: 'Steve and Jimbo are fighting for control the ball',
+        description: 'Steve and Jimbo are fighting for control the ball',
     },
     {
         title: 'Gorog',
         picSrc: 'pic02.jpg',
         thSrc: 'th02.jpg',
-        descripition: 'Gorog is fighting for control of his bike',
+        description: 'Gorog is fighting for control of his bike',
     },
     {
         title: 'Behind Tomas Daibis',
         picSrc: 'pic03.jpg',
         thSrc: 'th03.jpg',
-        descripition: 'Hardcourt Bike Polo game behind the statue of Tomas Daibis, College Green',
+        description: 'Hardcourt Bike Polo game behind the statue of Tomas Daibis, College Green',
     },
     {
         title: 'Wtf???',
         picSrc: 'pic04.jpg',
         thSrc: 'th04.jpg',
-        descripition: 'Spectators visibly can\'t beleive their eyes seeing that Young Mark just stole me bloody bike',
+        description: 'Spectators visibly can\'t beleive their eyes seeing that Young Mark just stole me bloody bike',
     },
     {
         title: 'Fiona',
         picSrc: 'pic05.jpg',
         thSrc: 'th05.jpg',
-        descripition: 'Fiona is moving in position to score another goal',
+        description: 'Fiona is moving in position to score another goal',
     },
     {
         title: 'Steel',
         picSrc: 'pic06.jpg',
         thSrc: 'th06.jpg',
-        descripition: 'Both the steel frames and the joint of the players are battle-hardened',
+        description: 'Both the steel frames and the joint of the players are battle-hardened',
     },
     {
         title: 'Jimbo down',
         picSrc: 'pic07.jpg',
         thSrc: 'th07.jpg',
-        descripition: 'Occasionaly one team or another have casualties',
+        description: 'Occasionaly one team or another have casualties',
     }
 
 ];
