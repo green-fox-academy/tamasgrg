319d3dcd823563d848d57407a76b05029533a7bb
tamasgrg <gorog@consolidity.com>
2020-06-25 19:33:06 +0200

    01-giphy done

--- 02-orientation/08-xhr/01-giphy/giphy.js
+++ 02-orientation/08-xhr/01-giphy/giphy.js
@@ -1,23 +1,26 @@
 'use strict';
 
-function getData() {
+function giphyApp() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=gwTIjlDbhiY0see8Z5k9N2c6yAwhlEjh&q=funny&limit=16&offset=0&rating=R&lang=en')
+  xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=gwTIjlDbhiY0see8Z5k9N2c6yAwhlEjh&q=funny&limit=16&offset=0&rating=R&lang=en');
   xhr.onload = () => {
     const response = JSON.parse(xhr.responseText);
     const thumbContainer = document.getElementById('thumb-container');
-    const picContainer = document.getElementById('pic-container');
-    
+
     for (let i = 0; i < response.data.length; i++) {
-      let imgUrl = response.data[i].images.fixed_height_small_still.url;
-      let image = document.createElement('img');
+      const imgUrl = response.data[i].images.fixed_height_small_still.url;
+      const image = document.createElement('img');
       image.src = imgUrl;
+      image.addEventListener('click', function() {
+        const pic = document.getElementById('pic');
+        pic.src = response.data[i].images.original.url;
+      });
       thumbContainer.appendChild(image);
-    }
+    };
   };
   xhr.send();
 };
 
 window.onload = () => {
-  getData();
+  giphyApp();
 };
