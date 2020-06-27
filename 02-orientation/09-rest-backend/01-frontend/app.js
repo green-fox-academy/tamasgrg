1307c2aa8c71413a28745b550c615c2c054154bc
tamasgrg <gorog@consolidity.com>
2020-06-27 14:23:20 +0200

    doubling done

--- 02-orientation/09-rest-backend/01-frontend/app.js
+++ 02-orientation/09-rest-backend/01-frontend/app.js
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  let input = parseInt(req.query.input);
+  res.json(double(input));
+});
+
+function double(num) {
+  if (!num) return {"error": "Please provide an input!"};
+  return {'received': num, 'result': num * 2};
+}
+
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
 });
