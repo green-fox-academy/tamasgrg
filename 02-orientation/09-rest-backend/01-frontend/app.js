76e9f5811dafcdea48741c2969ab77244aec66f0
tamasgrg <gorog@consolidity.com>
2020-06-27 15:54:28 +0200

    do-until done

--- 02-orientation/09-rest-backend/01-frontend/app.js
+++ 02-orientation/09-rest-backend/01-frontend/app.js
@@ -2,16 +2,19 @@
 
 const path = require('path');
 const express = require('express');
+const bodyParser = require("body-parser");
 const PORT = 8080;
 const app = express();
 
 app.use(express.static('assets'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/doubling', (req, res) => {
+//doubling
+  app.get('/doubling', (req, res) => {
   let input = parseInt(req.query.input);
   res.json(double(input));
 });
@@ -21,6 +24,7 @@ function double(num) {
   return {'received': num, 'result': num * 2};
 }
 
+//greeter
 app.get('/greeter', (req, res) => {
   let name = req.query.name;
   let title = req.query.title;
@@ -38,11 +42,40 @@ app.get('/greeter', (req, res) => {
   }
 });
 
+//append-a
 app.get('/appenda/:appendable', (req, res) => {
   let appendable = req.params.appendable;
-  if (!appendable) res.status(404);
+  // if (!appendable) res.status(404);
   res.json({"appended": `${appendable}a`});
 });
+
+//do-until
+app.post('/dountil/:action', (req, res) => {
+  let action = req.params.action;
+  let num = parseInt(req.body.until);
+  
+  if (!num) {
+    res.json({"error": "Please provide a number!"});
+  } else if (action == 'sum') {
+    res.json({"result": sum(num)});
+  } else if (action == 'factor') {
+    res.json({"result": factorio(num)});
+  }
+});
+
+function sum(num) {
+  for (let i = num - 1; i > 0; i--) {
+    num += i;
+  }
+  return num;
+}
+
+function factorio(num) {
+  for (let i = num - 1; i > 0; i--) {
+    num = num * i;
+  }
+  return num;
+}
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
