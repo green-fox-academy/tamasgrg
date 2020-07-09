1d24aa933c3f3d26586eb7031ab261ae488c4745
tamasgrg <gorog@consolidity.com>
2020-07-09 19:31:02 +0200

    first approach on 01-bookstore

---
+++ 02-orientation/10-sql-backend/01-bookstore/db.js
@@ -0,0 +1,16 @@
+// const mysql = require('mysql');
+
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'majom',
+//   // database: 'beers'
+// });
+
+// conn.connect((err) => {
+//   if (err) {
+//     console.error('Unable to connect to DB:', err);
+//     return;
+//   }
+//   console.log('Successfully connected to DB.');
+// });
