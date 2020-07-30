9f3f5865d30cfec172074bd11af85b4958ab6386
tamasgrg <gorog@consolidity.com>
2020-07-30 19:11:17 +0200

    promise-it-wont-hurt task03 done

--- 02-orientation/14-promise-and-fetch/01-promise-it-wont-hurt/02-fulfill.js
+++ 02-orientation/14-promise-and-fetch/01-promise-it-wont-hurt/02-fulfill.js
@@ -1,9 +1,17 @@
+// Create a promise. Have it fulfilled with a value of 'FULFILLED!' in
+// executor after a delay of 300ms, using setTimeout.
+
+// Then, print the contents of the promise after it has been fulfilled by passing
+// console.log to then.
+
 'use strict';
 
 let promise = new Promise((fulfill, reject) => {
   setTimeout(() => fulfill('FULFILLED!'), 300)
 });
 
-promise.then(fulfill => {
-  console.log(fulfill);
-});
+promise.then(console.log);
+
+// promise.then(fulfill => {
+//   console.log(fulfill);
+// });
