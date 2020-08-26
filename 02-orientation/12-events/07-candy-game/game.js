55e814d294615e3dc91b6f78ada7a1d091c4cb83
tamasgrg <gorog@consolidity.com>
2020-08-26 23:04:15 +0200

    events/07-candy-game done, far from perfect though

--- 02-orientation/12-events/07-candy-game/game.js
+++ 02-orientation/12-events/07-candy-game/game.js
@@ -6,17 +6,54 @@
 // - Display the candy producton rate in the `Candies / Second` row
 // - If you press the "make candy rain" button, the candy generation should speed up 10x
 
+
+// need to implement end-game logic, generating-speed and setIntervals aren't working really good
+
 'use strict';
 
 const buttons = document.getElementsByTagName('button');
 const candies = document.querySelector('.candies');
 const lollypops = document.querySelector('.lollypops');
 const speed = document.querySelector('.speed');
+let interval = 1000;
+let generating = false;
 
 buttons[0].addEventListener('click', () => candies.textContent++);
 buttons[1].addEventListener('click', () => {
   // if (candies.textContent >= 100) {
     candies.textContent -= 100;
     lollypops.textContent += "🍭";
+    generate();
   // }
 });
+buttons[2].addEventListener('click', () => {
+  if (generating === true) {
+    speed.textContent *= 10;
+    interval *= 0.1;
+    fastInterval();
+  }
+});
+
+let setUpInterval = setInterval(() => {
+  if (lollypops.textContent.length >= 20) increaseCandy();
+}, interval);
+
+function fastInterval() {
+  clearInterval(setUpInterval);
+  setInterval(() => increaseCandy(), interval);
+}
+
+function generate() {
+  if (lollypops.textContent.length >= 20 && generating === false) {
+    speed.textContent++;
+    generating = true;
+  }
+}
+
+function increaseCandy() {
+  if (candies.textContent > 9999) {
+    clearInterval(fastInterval);
+    alert('You won!');
+  }
+  candies.textContent++;
+}
