879850575e3ed60f93f48b90b46368cc235b87ee
tamasgrg <gorog@consolidity.com>
2020-08-28 16:11:57 +0200

    migrate-wip

--- 02-orientation/13-forms/04-newsletter/04-backend.js
+++ 02-orientation/13-forms/04-newsletter/04-backend.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+// const { send } = require('process');
 const PORT = 3000;
 const app = express();
 // const path = require('path');
@@ -10,6 +11,9 @@ app.listen(PORT, () => console.log(`The app is listening on ${PORT}`));
 
 app.post('/signup', (req, res) => {
   // Send back some HTML to the browser with the submitted data:
+  res.sendFile(__dirname + '/04-newsletter.html');
   // <h1>Thanks {username}, we will send updates to {email}</h1>
   // res.sendFile(path.resolve('./frontend/index.html'));
 });
+
+
