0b350d260ec868e1a9338b2b7510117099261587
tamasgrg <gorog@consolidity.com>
2020-08-30 18:34:50 +0200

    gallery-project done (w/o small triangles...)

--- 02-orientation/05-gallery-project/gallery/db.js
+++ 02-orientation/05-gallery-project/gallery/db.js
@@ -4,19 +4,19 @@ const picDb = [
         title: 'Sharp turn',
         picSrc: 'pic00.jpg',
         thSrc: 'th00.jpg',
-        description: 'Kropa is taking a sharp turn to lead an attack',
+        description: 'Kropa takes a sharp turn to lead an attack',
     },
     {
         title: 'Fight',
         picSrc: 'pic01.jpg',
         thSrc: 'th01.jpg',
-        description: 'Steve and Jimbo are fighting for controlling the ball',
+        description: 'Steve and Jimbo fight to control the ball',
     },
     {
         title: 'Gorog',
         picSrc: 'pic02.jpg',
         thSrc: 'th02.jpg',
-        description: 'Gorog is fighting for controlling his own bike',
+        description: 'Gorog fights to control his own bike',
     },
     {
         title: 'Behind Tomas Daibis',
@@ -28,13 +28,13 @@ const picDb = [
         title: 'Wtf?',
         picSrc: 'pic04.jpg',
         thSrc: 'th04.jpg',
-        description: 'Spectators visibly can\'t beleive their eyes, seeing how Young Mark just stole me bloody bike',
+        description: 'Spectators visibly can\'t beleive their eyes seeing how Young Mark just stole me bloody bike',
     },
     {
         title: 'Fiona',
         picSrc: 'pic05.jpg',
         thSrc: 'th05.jpg',
-        description: 'Fiona is moving in position to score another goal',
+        description: 'Fiona moves in position to score another goal',
     },
     {
         title: 'Steel',
--- 02-orientation/05-gallery-project/gallery/static/event.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const button = document.querySelector('header');
-  button.onclick = () => {
-  	alert('Green Fox!');
-	};
