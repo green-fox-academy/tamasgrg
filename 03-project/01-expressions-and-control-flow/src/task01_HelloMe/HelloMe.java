2775387384b06665b830efdf6856b54577fed7dd
tamasgrg <tjejtjusare@gmail.com>
2021-01-12 22:58:20 +0100

    organize repo

--- 03-project/01-expressions-and-control-flow/src/task01_HelloMe/HelloMe.java
+++ 03-project/01-expressions-and-control-flow/src/task01_HelloMe/HelloMe.java
@@ -1,4 +1,8 @@
 package task01_HelloMe;
 
 public class HelloMe {
+  public static void main(String[] args) {
+    // Modify this program to greet you instead of the World!
+    System.out.println("Hello, grg!");
+  }
 }
---
+++ 03-project/01-expressions-and-control-flow/src/task04_IntroduceYourself/IntroduceYourself.java
@@ -0,0 +1,21 @@
+package task04_IntroduceYourself;
+
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+    String name = "grg";
+    int age = 35;
+    float height = 1.82f;
+
+    System.out.println(name + "\n" + age + "\n" + height);
+  }
+}
